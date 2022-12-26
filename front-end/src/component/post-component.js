f364e7dc880dd66f3cd333f4695652acefa003e0
zolboo oktyabri <soulforger@zolboos-MacBook-Air-2.local>
2022-12-26 18:10:10 +0800

    instagram starts

---
+++ front-end/src/component/post-component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const PostComponent = () => {
+    return (
+        <div>
+            post
+        </div>
+    )
+}
---
+++ front-end/src/pages/home-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PostComponent } from '../component/post-component';
+
+export const HomePage = () => {
+    return (
+        <div>
+            Home
+            <PostComponent />
+        </div>
+    )
+}
