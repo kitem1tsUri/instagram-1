d3e469efa6d19d6ee76e39bc0762ec25b0785fdf
Myagmar Khuslen <myagmarkhuslen@Myagmars-MacBook-Pro.local>
2023-02-02 16:30:29 +0800

    prof added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from './pages/home-page';
+import { ProfilePage } from './pages/profile-page';
 import { SignInPage } from './pages/sign-in-page';
 import { SignUpPage } from './pages/sign-up-page';
 import './styles/App.css';
@@ -10,7 +11,9 @@ const App = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage/>}/>
+      <Route path="/sign-up" element={<SignUpPage />}/>
+      <Route path="/profile" element={<ProfilePage />}/>
+      {/* <Route path="/profile" element={<ProfilePage />}/> */}
     </Routes>
   </div>
   );
---
+++ front-end/src/pages/profile-page.js
@@ -0,0 +1,16 @@
+
+import "../styles/profile.css";
+
+export const ProfilePage = () => {
+    return ( 
+        <div className="ProfContainer"> 
+         <div>
+            <div className="left-cont">instagram</div>
+            <div className="middle-cont"> .
+                <div className="Prof-img"> </div>
+                <div className="posts"> posts</div>
+            </div>
+         </div>
+        </div>  
+    )
+}
